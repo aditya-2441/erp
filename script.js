713607ca15bbae01c9d056bbe980990874eac632
Aditya Singh <yt24541@gmail.com>
2025-09-09 23:20:44 +0530

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Show alert when clicking Notification box
+document.querySelectorAll(".box")[0].addEventListener("click", () => {
+  alert("You clicked on Notification!");
+});
+
+// Example: Show alert when clicking Notice box
+document.querySelectorAll(".box")[1].addEventListener("click", () => {
+  alert("You clicked on Notice!");
+});
